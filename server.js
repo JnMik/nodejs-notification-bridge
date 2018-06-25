b85821f33a6237f309a481cdaf25379a8cfebd78
jm <cyrjeanmichael@gmail.com>
2018-06-25 09:56:53 -0400

    Allow broadcasting on multiple sockets

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var
-  fs = require('fs'),
-  express = require('express'),
-  bodyParser = require('body-parser'),
-  app = express(),
-  config = require('./config');
+    fs = require('fs'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express(),
+    config = require('./config');
 
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 app.use(bodyParser.json());
 
@@ -15,15 +15,23 @@ var globalSocket;
 console.log('server listening on localhost:' + config.nodeJsListeningPort);
 
 app.get('/status', function (req, res) {
-  res.end('ok');
+    res.end('ok');
 });
 
 app.post('/notification', function (req, res) {
-  console.log('Push notification to listeners');
-  if(globalSocket) {
-    globalSocket.volatile.emit('notification', req.body);
-  }
-  res.end('ok');
+
+    //console.log('Push notification to listeners');
+
+    // forward information to all sockets
+    io.sockets.emit('notification', req.body);
+
+    /*
+    if (globalSocket) {
+        globalSocket.volatile.emit('notification', req.body);
+    }
+    */
+
+    res.end('ok');
 });
 
 var server = app.listen(config.nodeJsListeningPort);
@@ -32,7 +40,14 @@ var io = require('socket.io').listen(server);
 
 // Init a socket and emit every POST notification received to it
 // Every Web page connecting to this socket should be able to update his interface
-io.sockets.on('connection', function(socket) {
-  console.log('Init connection with client ');
-  globalSocket = socket;
+io.sockets.on('connection', function (socket) {
+
+    //console.log('Init connection with client ');
+    //console.log(' %s sockets connected', io.engine.clientsCount);
+
+    globalSocket = socket;
+
+    socket.on('disconnect', function () {
+        //console.log("disconnect: ", socket.id);
+    });
 });
